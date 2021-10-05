56a6190d409c35d5fc026ada726bb84bc468b780
Jekaterina <jekaterina.kassapova@gmail.com>
2021-10-05 21:20:57 +0300

    first commit

---
+++ DiceGame/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb 2 mängijat - arvuti (cpu) ja kasutaja (user)
+            //mõlemad mängijad viskavad täringuid;
+            //programm kontrollib, kumb mängija viskas rohkem
+            //mängija, kes viskab rohkem, ongi mängu võitja
+            //*täringuid visatakse 3 korda
+            //programm kuulutab võitjat
+
+            Random rnd = new Random();
+
+            //arvuti vise
+            int cpuRandom = rnd.Next(1, 7);
+
+            //kasutaja vise
+            int userRandom = rnd.Next(1, 7);
+
+            int cpuScore = 0;
+            int userScore = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}.");
+
+
+
+                if (cpuRandom < userRandom)
+                {
+                    Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
+                    userScore++;
+                }
+                else if (cpuRandom > userRandom)
+                {
+                    Console.WriteLine("Arvuti on mängu võitnud.");
+                    cpuScore++;
+                }
+                else
+                {
+                    Console.WriteLine("Viik!");
+                }
+
+            }
+
+                
+            
+
+            
+        }
+    }
+}
---
+++ ForLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I will not skateboard ot the halls.
+
+            //string message = "I will not skateboard ot the halls.".ToUpper();
+
+            for(int i = 0; i < 10; i++) //int i = 0; iteratsiooni algus; i < 10; iteratsiooni lõpp; i++ = i = i + 1 - samm
+            {
+                Console.WriteLine($"{i+1}. I will not skateboard ot the halls.".ToUpper());
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
