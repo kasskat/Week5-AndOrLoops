b7cb47c50a35c8e5d31355031dd42a2585187790
kasskat <90044410+kasskat@users.noreply.github.com>
2021-10-08 21:06:32 +0300

    Add files via upload

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -16,41 +16,48 @@ namespace DiceGame
             Random rnd = new Random();
 
             //arvuti vise
-            int cpuRandom = rnd.Next(1, 7);
+
 
             //kasutaja vise
-            int userRandom = rnd.Next(1, 7);
+
 
             int cpuScore = 0;
             int userScore = 0;
 
             for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}.");
+                int cpuRandom = rnd.Next(1, 7);
+                int userRandom = rnd.Next(1, 7);
 
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}.");
 
 
                 if (cpuRandom < userRandom)
                 {
-                    Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
                     userScore++;
                 }
                 else if (cpuRandom > userRandom)
                 {
-                    Console.WriteLine("Arvuti on mängu võitnud.");
                     cpuScore++;
                 }
-                else
-                {
-                    Console.WriteLine("Viik!");
-                }
+
+
+
 
             }
+            if (cpuScore > userScore)
+            {
+                Console.WriteLine("Arvuti on võitnud!");
 
-                
-            
-
-            
+            }
+            else if (cpuScore < userScore)
+            {
+                Console.WriteLine("Kasutaja on võitnud!");
+            }
+            else
+            {
+                Console.WriteLine("Viik!");
+            }
         }
     }
 }
